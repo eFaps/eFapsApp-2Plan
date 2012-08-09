a5ad3aa522c26233880e49c6b6252ec23e03f32e
m.aranya@moxter.net <m.aranya@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2012-08-09 21:43:18 +0000

    - remove annotations.
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/2plan/trunk@7896 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/efaps/ESJP/org/efaps/esjp/twoplan/jaxb/package-info.java
+++ src/main/efaps/ESJP/org/efaps/esjp/twoplan/jaxb/package-info.java
@@ -4,7 +4,5 @@
  * @author The eFaps Team
  * @version $Id$
  */
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.2b-intime.com/model",
-                elementFormDefault = javax.xml.bind.annotation.XmlNsForm.UNSET)
 package org.efaps.esjp.twoplan.jaxb;
 
